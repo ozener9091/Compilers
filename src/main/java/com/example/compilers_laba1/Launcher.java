6491f5f725426739f120cd9d765d402c96fb33df
Vlad <ruberlep34@bk.ru>
2026-02-12 21:37:02 +0700

    add menu

--- src/main/java/com/example/compilers_laba1/Launcher.java
+++ src/main/java/com/example/compilers_laba1/Launcher.java
@@ -3,7 +3,7 @@ package com.example.compilers_laba1;
 import javafx.application.Application;
 
 public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+    static void main(String[] args) {
+        Application.launch(Main.class, args);
     }
 }
