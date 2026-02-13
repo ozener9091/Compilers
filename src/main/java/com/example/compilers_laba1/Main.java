9530b8b70d369f1199a2ace0bbe58d75f3672546
Vlad <ruberlep34@bk.ru>
2026-02-13 16:20:14 +0700

    add localization

--- src/main/java/com/example/compilers_laba1/Main.java
+++ src/main/java/com/example/compilers_laba1/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Compiler Laba1");
+        stage.setTitle("Compiler Laboratory #1");
         stage.setScene(scene);
         stage.show();
     }
