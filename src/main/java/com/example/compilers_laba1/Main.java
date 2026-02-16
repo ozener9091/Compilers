5f437382abcc91dc13284cf0b6689edde5c8b198
Vlad <ruberlep34@bk.ru>
2026-02-16 19:06:01 +0700

    add highlight service

--- src/main/java/com/example/compilers_laba1/Main.java
+++ src/main/java/com/example/compilers_laba1/Main.java
@@ -6,12 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("compilers-main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/editor.css")).toExternalForm());
         stage.setTitle("Compiler Laboratory #1");
         stage.setScene(scene);
         stage.show();
